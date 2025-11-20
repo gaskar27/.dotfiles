5e9705728a2229957a2cc271231df2440aa985b9
Gaskar <anjarazafinjato@gmail.com>
2025-11-20 15:34:56 +0300

    add fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Set up fzf key bindings
+fzf --fish | source
+zoxide init --cmd cd fish | source
+
+starship init fish | source
